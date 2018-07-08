0e48845bb4f32b7aaa20d1c9a39ef234001bc942
Christine Bonk <christinebonk@gmail.com>
2018-07-07 22:13:46 -0400

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bamazon
+
+## Description
+
+A simple store application where customers can purchase goods and managers can run their business.
+
+## Video
+
+https://drive.google.com/file/d/1uMFh2YAVJy_j0GBLmm3aOF52Xrk0tu1A/view
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,13 +8,6 @@ var con = mysql.createConnection({
 	database: "bamazon"
 });
 
-
-// var tbl = "tablefun";
-// var get = "SELECT * FROM table " + tbl;
-// var insert = "INSERT INTO " + tbl + "(hello,hi) values ('good', true)";
-// var upd = "UPDATE " + tbl + " SET hello = 'pancakes' WHERE id = 3";
-// var dlt = "DELETE FROM " + tbl + " WHERE id = 1";
-
 con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT item_id, product_name, price FROM products", function (err, result, fields) {
@@ -66,22 +59,15 @@ function howMany(order, product) {
 
 function checkProduct(number, product, order) {
 	con.query(`SELECT stock_quantity FROM products WHERE item_id=${order}`, function(err, result) {
-		//error handling
 		if(err) {
 					console.log(err)
 		}
-		//define variables
 		var quantity = parseInt(result[0].stock_quantity);
-		//check stock
 		if(number > quantity) {
 			console.log("Sorry it looks like we are out of stock!");
-			// if (quantity > 0) {
-			// 	console.log("Would you like " + quantity + " " + product + "s instead?")
-			// }
 		} else {
 			console.log("We have enough in stock! Let me get your order ready!")
 			var remaining = quantity - number;
-			//update stock
 			con.query(`UPDATE products SET stock_quantity=${remaining} WHERE item_id=${order}`, function(err, result) {
 				if(err) {
 					console.log(err)
